467224f38fb4cfda3e08ad3a63036bffa863148f
yoniu <xu_jingzhi@200011.net>
2021-07-29 23:30:16 +0800

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,23 @@ function init(){
             $(this).wrap('<a class="post-imgLink" href="' + this.src + '" data-caption="' + this.alt + '"></a>');
         }
 	});
+    $("article .photosets").each(function(){
+        var count = $(this).find("a.post-imgLink").length;
+        switch(count){
+            case 1:
+                $(this).addClass("photosets-1");
+                break;
+            case 2:
+                $(this).addClass("photosets-2");
+                break;
+            case 4:
+                $(this).addClass("photosets-4");
+                break;
+            default:
+                $(this).addClass("photosets-3");
+        }
+        console.log(count);
+    });
     lightGallery(document.getElementsByTagName('article')[0], {
 		selector: '.post-imgLink',
 		share: false,
